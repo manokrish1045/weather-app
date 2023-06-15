1bd4abe4420b1f060e40988cec47b89b01615b69
Mano <109267208+manokrish1045@users.noreply.github.com>
2023-06-15 04:50:51 +0000

    Update 2 files css

--- src/Home.js
+++ src/Home.js
@@ -1,44 +1,45 @@
-import { useState } from 'react';
-import { Box, styled } from '@mui/material';
-import Sunset from './assets/images/bg.jpg';
-import Form from './components/Form';
-import Information from './components/Information';
-import { useNavigate } from 'react-router-dom';
-
+import { useState } from "react";
+import { Box, styled } from "@mui/material";
+import Sunset from "./assets/images/bg.jpg";
+import Form from "./components/Form";
+import Information from "./components/Information";
+import { useNavigate } from "react-router-dom";
+import "./Home.css";
 const Component = styled(Box)({
-    height: '100vh',
-    display: 'flex',
-    alignItems: 'center',
-    margin: '0 auto',
-    width: '65%'
-})
+  height: "100vh",
+  display: "flex",
+  alignItems: "center",
+  margin: "0 auto",
+  width: "65%",
+});
 
 const Image = styled(Box)({
-    backgroundImage: `url(${Sunset})`,
-    width: '27%',
-    height: '80%',
-    backgroundSize: 'cover',
-    borderRadius: '20px 0 0 20px'
-})
+  backgroundImage: `url(${Sunset})`,
+  width: "27%",
+  height: "80%",
+  backgroundSize: "cover",
+  borderRadius: "20px 0 0 20px",
+});
 
 const Home = () => {
-    const [result, setResult] = useState({})
-    const navigate = useNavigate()
-    const logOut = () => {
-        window.localStorage.clear();
-        window.location.href = "./login";
-    };
-    return (
-        <Component>
-            <Image></Image>
-            <Box style={{ width: '73%', height: '80%' }}>
-                <Form setResult={setResult} />
-                <Information result={result} />
-            </Box>
-            <button onClick={logOut}>Logout</button>
-        </Component>
-
-    )
-}
+  const [result, setResult] = useState({});
+  const navigate = useNavigate();
+  const logOut = () => {
+    window.localStorage.clear();
+    window.location.href = "./login";
+  };
+  return (
+    <Component>
+      <Image></Image>
+      <Box style={{ width: "73%", height: "80%" }}>
+        <Form setResult={setResult} />
+        <Information result={result} />
+      </Box>
+      <button className="button1" onClick={logOut}>
+        Logout
+      </button>
+    </Component>
+  );
+};
 
 export default Home;
